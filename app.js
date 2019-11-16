9d4d3199b5625d9c445804a14dbd313add97a262
lorena <lorenalies2312@gmail.com>
2019-11-16 11:20:21 -0500

    addign middeware to the app

--- app.js
+++ app.js
@@ -13,37 +13,10 @@ app.use(cookieParser());
 // middeware
 app.use(express.static("./public"));
 
+const mainRoutes = require('./routes');
+app.use(mainRoutes);
 //routs
 
-
-
-
-
-
-
-/*
-const mainRoutes = require('./routes');
-const cardRoutes = require('./routes/cards');
-
-app.use(mainRoutes);
-app.use('/cards', cardRoutes);
-
-app.post("/welcome", function(req, res) {
-  console.log(req.body.taskItem);
-  list.push(req.body.taskItem);
-  res.render("main.ejs", { list: list });
-});
-app.use((req, res, next) => {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-app.use((err, req, res, next) => {
-  res.locals.error = err;
-  res.status(err.status);
-  res.render('error');
-});*/
 
   app.listen(3004, function(err) {
     if (err) console.log(err);
