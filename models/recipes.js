f99a272b7258e6a9aa9e35d6164efb213ddc7887
Roger <evolpirate@gmail.com>
2019-11-21 11:49:35 -0500

    added recipes model, added routes to create a recipe , added route to query all recipes, added recipes add view to add form

---
+++ models/recipes.js
@@ -0,0 +1,28 @@
+//create a model for our class
+
+//exporting this model to our index
+module.exports = function(sequelize, DataTypes) {
+  var Recipes = sequelize.define("Recipes", {
+    //define columns of our table
+    name: {
+      type: DataTypes.STRING
+    },
+    diet: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    description: {
+      type: DataTypes.STRING
+    },
+    ingredients: {
+        type: DataTypes.STRING
+      },
+    iamge: {
+    type: DataTypes.STRING
+    },
+    userID: {
+        type: DataTypes.STRING
+    }
+  });
+  return Recipes;
+};
